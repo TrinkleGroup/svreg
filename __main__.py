Fix softplus, weighted average, sorting, and logs

__main__.py
@@ -97,7 +97,11 @@ def main(client, settings):
     regressor = SVRegressor(settings, database)
     archive = Archive(os.path.join(settings['outputPath'], 'archive'))
 
-    costFxn = buildCostFunction(settings, len(database.attrs['natoms']))
+    costFxn = buildCostFunction(
+        settings,
+        len(database.attrs['natoms']),
+        sum(database.attrs['natoms'].values())
+    )
 
     # Begin symbolic regression
     if args.trees is not None:
@@ -347,7 +351,12 @@ def polish(client, settings):
         evaluator = SVEvaluator(database, settings)
 
     regressor = SVRegressor(settings, database)
-    costFxn = buildCostFunction(settings, len(database.attrs['natoms']))
+
+    costFxn = buildCostFunction(
+        settings,
+        len(database.attrs['natoms']),
+        sum(database.attrs['natoms'].values())
+    )
 
     if args.trees is not None:
         with open(args.trees, 'r') as f:
@@ -394,7 +403,6 @@ def polish(client, settings):
     from svreg.archive import Entry
 
     entries = {md5Hash(t): Entry(t, savePath) for t in regressor.trees}
-    prevBestCosts = {md5Hash(t): np.inf for t in regressor.trees}
 
     import pickle
 
@@ -471,55 +479,52 @@ def polish(client, settings):
 
             entry = entries[treeName]
 
-            if opt.result.fbest < prevBestCosts[treeName]:
-                prevBestCosts[treeName] = opt.result.fbest
+            bestIdx = np.argmin(costs[0])
+            entry.bestIdx = bestIdx
+            entry.cost = costs[bestIdx]
+            entry.bestParams = rawPopulations[0][bestIdx]
+            entry.bestErrors = errors[0][bestIdx]
 
-                sums = np.sum(costs[0], axis=1)
-                bestIdx = np.argmin(sums)
-                entry.cost = costs[bestIdx]
-                entry.bestParams = rawPopulations[0][bestIdx]
-                entry.bestErrors = errors[0][bestIdx]
-
-                pickle.dump(
-                    entry,
-                    open(
-                        os.path.join(savePath, treeName, 'entry.pkl'),
-                        'wb'
-                    )
+            pickle.dump(
+                entry,
+                open(
+                    os.path.join(savePath, treeName, 'entry.pkl'),
+                    'wb'
                 )
+            )
 
-                pickle.dump(
-                    opt,
-                    open(
-                        os.path.join(savePath, treeName, 'opt.pkl'),
-                        'wb'
-                    )
+            pickle.dump(
+                opt,
+                open(
+                    os.path.join(savePath, treeName, 'opt.pkl'),
+                    'wb'
                 )
+            )
 
-                pickle.dump(
-                    energies,
-                    open(
-                        os.path.join(savePath, treeName, 'energies.pkl'),
-                        'wb'
-                    )
+            pickle.dump(
+                energies,
+                open(
+                    os.path.join(savePath, treeName, 'energies.pkl'),
+                    'wb'
                 )
+            )
 
 
-                pickle.dump(
-                    forces,
-                    open(
-                        os.path.join(savePath, treeName, 'forces.pkl'),
-                        'wb'
-                    )
+            pickle.dump(
+                forces,
+                open(
+                    os.path.join(savePath, treeName, 'forces.pkl'),
+                    'wb'
                 )
+            )
 
-                pickle.dump(
-                    tree,
-                    open(
-                        os.path.join(savePath, treeName, 'tree.pkl'),
-                        'wb'
-                    )
+            pickle.dump(
+                tree,
+                open(
+                    os.path.join(savePath, treeName, 'tree.pkl'),
+                    'wb'
                 )
+            )
 
 
 ################################################################################
@@ -547,7 +552,7 @@ def printTreeCosts(optStep, costs, penalties, startTime):
     )
 
 
-def buildCostFunction(settings, numStructs):
+def buildCostFunction(settings, numStructs, totalNumAtoms):
     """
     A function factory for building different types of cost functions. Assumes
     that the cost function will take in a list of (P, S) arrays, where P is the
@@ -571,14 +576,22 @@ def buildCostFunction(settings, numStructs):
 
         costs = []
         for treeErr in errors:
-            costs.append(np.average(np.multiply(treeErr, scaler), axis=1))
+            # costs.append(np.average(np.multiply(treeErr, scaler), axis=1))
+            cpy = treeErr.copy()
+            cpy[:, 1::2] /= totalNumAtoms
+            costs.append(np.multiply(cpy, scaler))
 
         return costs
 
     def rmse(errors):
         costs = []
         for treeErr in errors:
-            costs.append(np.sqrt(np.average(np.multiply(treeErr, scaler)**2, axis=1)))
+            # costs.append(np.sqrt(np.average(np.multiply(treeErr, scaler)**2, axis=1)))
+            cpy = treeErr.copy()
+            cpy[:, 1::2] /= totalNumAtoms
+            costs.append(np.sqrt(np.multiply(cpy, scaler)))
+
+        return costs
 
     t = settings['huberThresh']
     def huber(errors):

svreg/evaluator.py
@@ -12,6 +12,21 @@ class SVEvaluator:
         self.database   = database
         self.settings   = settings
 
+        if settings['costFxn'] == 'MAE':
+            def f(c, t):
+                return np.average(abs(c-t), axis=(1,2))
+            self.fErr = f
+        elif settings['costFxn'] == 'RMSE':
+            def f(c, t):
+                return np.average((c-t)**2, axis=(1,2))
+            self.fErr = f
+        else:
+            raise RuntimeError(
+                "SVEvaluator hasn't implemented {} yet.".format(
+                    settings['costFx']
+                )
+            )
+
     # def evaluate(self, trees, database, fullPopDict, P, allSums=False, useDask=True, useGPU=False):
     def evaluate(self, trees, fullPopDict, P, numTasks, allSums=False, useDask=True, useGPU=False):
         if useGPU:
@@ -98,10 +113,13 @@ class SVEvaluator:
                         trueForces = worker._true_forces[struct]
 
                         engResult, fcsResult = tree.eval(useDask=False, allSums=allSums)
+                        
+                        fcsErrors = self.fErr(sum(fcsResult), trueForces)
+                        fcsErrors *= Na
 
-                        fcsErrors = np.average(
-                            abs(sum(fcsResult) - trueForces), axis=(1,2)
-                        )
+                        # fcsErrors = np.average(
+                        #     abs(sum(fcsResult) - trueForces), axis=(1,2)
+                        # )
 
                         treeResults.append([sum(engResult), fcsErrors])
 

svreg/functions.py
@@ -114,6 +114,13 @@ def _derivative_sigmoid(x):
     """Chain rule on sig() = sig'()*(dx/dr), where sig'() = sig()*(1-sig())"""
     return (sigmoid(x[0])*(1-sigmoid(x[0])))[:, :, np.newaxis, np.newaxis]*x[1]
 
+def splus2(x):
+    print('x:', x)
+    return np.log(1 + np.exp(-np.abs(x))) + np.maximum(x, 0)
+
+def _derivative_splus2(x):
+    expx = np.exp(-np.abs(x[0]))
+    return (-expx/(1+expx))[:, :, np.newaxis, np.newaxis]*x[1] + np.maximum(x[1], 0)
 
 #@dask.delayed
 def splus(x):
@@ -157,7 +164,7 @@ _derivative_map = {
     # 'tan': _derivative_tan,
     'exp': _derivative_exp,
     'sig': _derivative_sigmoid,
-    'softplus': _derivative_splus,
+    'softplus': _derivative_splus2,
 }
 
 
@@ -194,7 +201,7 @@ log1  = _Function(function=_protected_log, name='log', arity=1)
 # arctan1  = _Function(function=np.arctan, name='arctan', arity=1)
 exp   = _Function(function=np.exp, name='exp', arity=1)
 sig   = _Function(function=sigmoid, name='sig', arity=1)
-softplus = _Function(function=splus, name='softplus', arity=1)
+softplus = _Function(function=splus2, name='softplus', arity=1)
 
 _function_map = {
     'add': add2,